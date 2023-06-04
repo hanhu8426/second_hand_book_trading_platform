03283c52308f53eda7ce8394c484370009f34b78
HEIHEICafe <1617117064@qq.com>
2023-06-04 23:43:44 +0800

    修改url之后的提交

--- front/src/api/address.js
+++ front/src/api/address.js
@@ -1,6 +1,6 @@
 import ajax from "./ajax";
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'http://8.140.52.224:8080'
 
 //添加地址
 export const reqAddAddress = (address) => ajax(BASE_URL+'/addUserAddress', {
